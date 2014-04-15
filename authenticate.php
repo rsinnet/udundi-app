bec32d30c12ce925710d95a4657c7f14f8b35002
Ryan W. Sinnet <ryan@rwsinnet.com>
2014-04-14 22:48:15 -0500

    Working on authentication

--- authenticate.php
+++ authenticate.php
@@ -12,18 +12,9 @@ $con = udundi_sql_connect();
 // DO AUTHENTICATION HERE!
 
 // Make sure the id is not a duplicate. This is unlikely. Also store in database.
-//while (!add_session_to_database($con, session_id()))
-//    session_regenerate_id();
+while (!add_session_to_database($con, session_id()))
+    session_regenerate_id();
 
-//redirect_to_home();
+redirect_to_home();
 
 ?>
-<html>
-  <body>
-    <?php
-      echo session_id();
-      echo "<br>";
-      echo add_session_to_database($con, session_id());
-       ?>
-  </body>
-</html>
