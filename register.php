8bc5f166736537d397f1098045b4fb5b9e985ca7
Ryan W. Sinnet <ryan@rwsinnet.com>
2014-04-15 15:02:54 -0500

    Changing register form to POST.

--- register.php
+++ register.php
@@ -80,7 +80,7 @@
       <div class="col-xs-12 col-sm-12 col-md-5 col-lg-5">
 	<div class="well no-padding">
 	  
-	  <form action="php/demo-register.php" id="smart-form-register" class="smart-form client-form">
+	  <form action="php/demo-register.php" method="POST" id="smart-form-register" class="smart-form client-form">
 	    <header>
 	      Registration is FREE*
 	    </header>
