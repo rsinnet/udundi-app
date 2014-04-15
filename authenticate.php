0057d4eea93558859332aa38e4e87fc5a4e251ca
Ryan W. Sinnet <ryan@rwsinnet.com>
2014-04-15 16:35:03 -0500

    Moving secure data to separate database.

--- authenticate.php
+++ authenticate.php
@@ -1,37 +1,38 @@
 <?php
-  
-  require_once("lib/password.php");
-  require_once("inc/utilities.php");
-  
-  // Get a session id.
-  if (session_id() == '')
-      session_start();
-  
-  if (empty($_POST['email']) || empty($_POST['password']) )
-      login_error();
-  
-  // Connect to the database.
-  $con = udundi_sql_connect();
-  
-  // DO AUTHENTICATION HERE!
-  // Get the hash from the database.
-  $sql_command = "SELECT password FROM users WHERE email=\"" . $_POST['email'] . "\"";
-  $result = $con->query($sql_command);
-  
-  if ($row = $result->fetch_array()) {
-      if (password_verify($_POST['password'], $row['password']))
-      {
-          // Make sure the id is not a duplicate. This is unlikely. Also store in database.
-          while (!add_session_to_database($con, session_id()))
-              session_regenerate_id();
-          redirect_to_home();
-      }
-      else
-          login_error();
-  }
-  else
-      login_error();
-  
-  $result->close();
-  
+
+require_once("lib/password.php");
+require_once("inc/utilities.php");
+require_once("inc/secure.php");
+
+// Get a session id.
+if (session_id() == '')
+    session_start();
+
+if (empty($_POST['email']) || empty($_POST['password']) )
+    login_error();
+
+// Connect to the database.
+$con = udundi_secure_sql_connect();
+
+// DO AUTHENTICATION HERE!
+// Get the hash from the database.
+$sql_command = "SELECT password FROM users_secure WHERE email=\"" . $_POST['email'] . "\"";
+$result = $con->query($sql_command);
+
+if ($row = $result->fetch_array()) {
+    if (password_verify($_POST['password'], $row['password']))
+    {
+// Make sure the id is not a duplicate. This is unlikely. Also store in database.
+        while (!add_session_to_database($con, session_id()))
+            session_regenerate_id();
+        redirect_to_home();
+    }
+    else
+        login_error();
+}
+else
+    login_error();
+
+$result->close();
+
 ?>
---
+++ inc/secure.php
@@ -0,0 +1,13 @@
+<?php
+function udundi_secure_sql_connect()
+{
+    $dbuser = 'rsinnet_authuser';
+    $dbpass = 'p{}]~H7+em<yBtC';
+    $con = mysqli_connect("localhost", $dbuser, $dbpass, "rsinnet_udundi_secure");
+    if (mysqli_connect_errno())
+    {
+        echo "Failed to connect: " . mysqli_connect_error();
+        // redirect to error page.
+    }
+    return $con;
+}
