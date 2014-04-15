d65528ab8d70040c5c51483b0736c9c1c75952ba
Ryan W. Sinnet <ryan@rwsinnet.com>
2014-04-15 13:32:25 -0500

    Removing include guard in utilities.php.

--- inc/utilities.php
+++ inc/utilities.php
@@ -1,45 +1,41 @@
 <?php
-if (!isset($__INCLUDE_UTILITIES_PHP))
+function udundi_sql_connect()
 {
-    function udundi_sql_connect()
+    $dbuser = 'rsinnet_webuser';
+    $dbpass = 'Z?Z07uwL#(4g';
+    $con = mysqli_connect("localhost", $dbuser, $dbpass, "rsinnet_udundi");
+    if (mysqli_connect_errno())
     {
-        $dbuser = 'rsinnet_webuser';
-        $dbpass = 'Z?Z07uwL#(4g';
-        $con = mysqli_connect("localhost", $dbuser, $dbpass, "rsinnet_udundi");
-        if (mysqli_connect_errno())
-        {
-            echo "Failed to connect: " . mysqli_connect_error();
-            // redirect to error page.
-        }
-        return $con;
+        echo "Failed to connect: " . mysqli_connect_error();
+        // redirect to error page.
     }
-    
-    function add_session_to_database($con, $session_id)
-    {
-        $user_id = 1;
-        $sql_command = "INSERT INTO sessions (id, userid) ".
-                       "VALUES (\"" . $session_id . "\", \"" . $user_id."\")";
-        return $con->query($sql_command);
-    }
-
-    function redirect_to_login()
-    {
-        $public_space = array("login.php", "register.php");
-        if (!in_array(basename($_SERVER['REQUEST_URI']), $public_space))
-            header('Location: http://dev.iamphilosopher.com/login.php');
-    }
-
-    function redirect_to_home()
-    {
-        header('Location: http://dev.iamphilosopher.com/index.php');
-    }
-
-    function conditional_redirect_from_public_area()
-    {
-        $public_space = array("login.php", "register.php");
-        if (in_array(basename($_SERVER['REQUEST_URI']), $public_space))
-            redirect_to_home();
-    }
+    return $con;
 }
-$__INCLUDE_UTILITIES_PHP = 1;
+    
+function add_session_to_database($con, $session_id)
+{
+    $user_id = 1;
+    $sql_command = "INSERT INTO sessions (id, userid) ".
+                   "VALUES (\"" . $session_id . "\", \"" . $user_id."\")";
+    return $con->query($sql_command);
+}
+
+function redirect_to_login()
+{
+    $public_space = array("login.php", "register.php");
+    if (!in_array(basename($_SERVER['REQUEST_URI']), $public_space))
+        header('Location: http://dev.iamphilosopher.com/login.php');
+}
+
+function redirect_to_home()
+{
+    header('Location: http://dev.iamphilosopher.com/index.php');
+}
+
+function conditional_redirect_from_public_area()
+{
+    $public_space = array("login.php", "register.php");
+    if (in_array(basename($_SERVER['REQUEST_URI']), $public_space))
+        redirect_to_home();
+}
 ?>
