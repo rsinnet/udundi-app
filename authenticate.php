91a121b3df1ae3ac507b6c4dfac2857adc4f62c4
Ryan W. Sinnet <ryan@rwsinnet.com>
2014-04-15 15:30:56 -0500

    Testing password verification.

--- authenticate.php
+++ authenticate.php
@@ -1,38 +1,48 @@
 <?php
-
-require_once("lib/password.php");
-require_once("inc/utilities.php");
-
-// Get a session id.
-if (session_id() == '')
-    session_start();
-
-if (empty($_POST['email']) || empty($_POST['password']) )
-    login_error();
-
-// Connect to the database.
-$con = udundi_sql_connect();
-
-// DO AUTHENTICATION HERE!
-// Get the hash from the database.
-$sql_command = "SELECT password FROM users WHERE email=\"" . $_POST['email'] . "\"";
-$result = $con->query($sql_command);
-
-if ($row = $result->fetch_array()) {
-    if (password_verify($POST_['password'], $row['password']))
-    {
-        // Make sure the id is not a duplicate. This is unlikely. Also store in database.
-        while (!add_session_to_database($con, session_id()))
-            session_regenerate_id();
-        redirect_to_home();
-    
-    }
-    else
-        login_error();
-}
-else
-    login_error();
-
-$result->close();
-
+  
+  require_once("lib/password.php");
+  require_once("inc/utilities.php");
+  
+  // Get a session id.
+  if (session_id() == '')
+      session_start();
+  
+  if (empty($_POST['email']) || empty($_POST['password']) )
+      login_error();
+  
+  // Connect to the database.
+  $con = udundi_sql_connect();
+  
+  // DO AUTHENTICATION HERE!
+  // Get the hash from the database.
+  $sql_command = "SELECT password FROM users WHERE email=\"" . $_POST['email'] . "\"";
+  $result = $con->query($sql_command);
+  
+  $password = $_POST['password'];
+  if ($row = $result->fetch_array()) {
+      $hash = $row['password'];
+      if (password_verify($POST_['password'], $row['password']))
+      {
+  // Make sure the id is not a duplicate. This is unlikely. Also store in database.
+          while (!add_session_to_database($con, session_id()))
+              session_regenerate_id();
+  //redirect_to_home();
+          
+      }
+  //else
+  //        login_error();
+  }
+  //else
+  //    login_error();
+  
+  $result->close();
+  
 ?>
+
+<html>
+  <body>
+    <?php
+      echo $password;
+    ?>
+  </body>
+</html>
