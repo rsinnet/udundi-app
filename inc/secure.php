8a80f02e37794b5f4060a1b41ffcd2cc5f0c1f2d
Ryan W. Sinnet <ryan@rwsinnet.com>
2014-04-15 17:41:44 -0500

    Working on demo-register.php.

--- inc/secure.php
+++ inc/secure.php
@@ -1,4 +1,6 @@
 <?php
+// This page is only accessed when writing to the secure database.
+
 function udundi_secure_sql_connect()
 {
     $dbuser = 'rsinnet_authuser';
@@ -10,6 +12,35 @@ function udundi_secure_sql_connect()
         // redirect to error page.
     }
     return $con;
+}
+
+
+// Took the following functions from
+//   http://stackoverflow.com/questions/2593807/md5uniqid-makes-sense-for-random-unique-tokens
+// This should give the most random tokens possible, probably drawing from /dev/urandom.
+function crypto_rand_secure($min, $max) {
+    $range = $max - $min;
+    if ($range < 0) return $min; // not so random...
+    $log = log($range, 2);
+    $bytes = (int) ($log / 8) + 1; // length in bytes
+    $bits = (int) $log + 1; // length in bits
+    $filter = (int) (1 << $bits) - 1; // set all lower bits to 1
+    do {
+        $rnd = hexdec(bin2hex(openssl_random_pseudo_bytes($bytes)));
+        $rnd = $rnd & $filter; // discard irrelevant bits
+    } while ($rnd >= $range);
+    return $min + $rnd;
+}
+
+function get_activation_token($length=100){
+    $token = "";
+    $codeAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    $codeAlphabet.= "abcdefghijklmnopqrstuvwxyz";
+    $codeAlphabet.= "0123456789";
+    for($i=0;$i<$length;$i++){
+        $token .= $codeAlphabet[crypto_rand_secure(0,strlen($codeAlphabet))];
+    }
+    return $token;
 }
 
 ?>
--- php/demo-register.php
+++ php/demo-register.php
@@ -1,14 +1,61 @@
-<html>
-  <body>
-    <?php
-echo "hey";
-      if (isset($_POST['email']) && isset($_POST['password']) )
-      {
-          echo $password;
-          echo "<br>";
-          require_once('../lib/password.php');
-          echo password_hash($_POST['password'], PASSWORD_BCRYPT);
-      }
-    ?>
-  </body>
-</html>
+<?php
+
+require_once('../php/utilities.php');
+require_once('../php/secure.php');
+
+if (isset($_POST['email']) &&
+    isset($_POST['password']) &&
+    isset($_POST['confirmPassword']) &&
+    isset($_POST['terms']))
+{
+    $email = $_POST['email'];
+
+    if ($_POST['terms'] &&
+        ($_POST['password'] == $_POST['confirmPassword']))
+    {
+
+        // Connect to both databases.
+        $con = udundi_sql_connect();
+        $scon = udundi_secure_sql_connect();
+
+        // Hash the password with bcrypt.
+        require_once('../lib/password.php');
+        $hash = password_hash($_POST['password'], PASSWORD_BCRYPT);
+
+
+        // Add user to database, but with account not activated.
+        $sql_command = "INSERT INTO users_secure (email, password) VALUES (\"$email\", \"$hash\")";
+//$scon->query($sql_command);
+//$scon->close();
+//unset($hash);
+        echo $sql_command;
+        echo "<br>";
+        
+        $sql_command = "INSERT INTO users (email) VALUES (\"$email\")";
+        echo $sql_command;
+        echo "<br>";
+
+        // Generate an activation token.
+        $token = get_activation_token();
+        echo $token;
+        echo "<br>";
+
+        // Send activation email to registrant.
+
+        // Add activation code to database with expiration time three days from now.
+
+        // Going to need a cronjob or something to clean up the database.
+
+        // Display thank you page.
+        echo $password;
+        echo "<br>";
+        echo $token;
+    }
+    else
+    {
+        // Redirect to login page, preferably filling in certain values by passing
+        // POST data.
+        redirect_to_login)(;
+    }
+}
+?>
