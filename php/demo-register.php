5f6075d2d8172225766a3902126f4f1df0406633
Ryan W. Sinnet <ryan@rwsinnet.com>
2014-04-15 14:55:31 -0500

    Putting password hasher into demo_register.php.

--- php/demo-register.php
+++ php/demo-register.php
@@ -1,6 +1,11 @@
-<?php
-if( isset($_POST['submit']) )
-{
-	// Your PHP code here
-}
-?>
+<html>
+  <body>
+    <?php
+      if ( isset($_POST['submit']) && isset($_POST['id']) && isset($_POST['password']) )
+      {
+          require_once('lib/password.php');
+          echo password_hash($_POST['password'], PASSWORD_BCRYPT);
+      }
+    ?>
+  </body>
+</html>
