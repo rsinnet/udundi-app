5b0352f38dc6b3a9bda47857c69212bad0573ef4
Ryan W. Sinnet <ryan@rwsinnet.com>
2014-04-15 15:48:52 -0500

    Testing password verification.

--- authenticate.php
+++ authenticate.php
@@ -1,56 +1,37 @@
 <?php
   
-require_once("lib/password.php");
-require_once("inc/utilities.php");
+  require_once("lib/password.php");
+  require_once("inc/utilities.php");
   
-// Get a session id.
-if (session_id() == '')
-    session_start();
+  // Get a session id.
+  if (session_id() == '')
+      session_start();
   
-if (empty($_POST['email']) || empty($_POST['password']) )
-    login_error();
+  if (empty($_POST['email']) || empty($_POST['password']) )
+      login_error();
   
-// Connect to the database.
-$con = udundi_sql_connect();
+  // Connect to the database.
+  $con = udundi_sql_connect();
   
-// DO AUTHENTICATION HERE!
-// Get the hash from the database.
-$sql_command = "SELECT password FROM users WHERE email=\"" . $_POST['email'] . "\"";
-$result = $con->query($sql_command);
- 
-$password = $_POST['password'];
-
-if ($row = $result->fetch_array()) {
-    $hash = $row['password'];
-//else
-//        login_error();
-}
-//else
-//    login_error();
+  // DO AUTHENTICATION HERE!
+  // Get the hash from the database.
+  $sql_command = "SELECT password FROM users WHERE email=\"" . $_POST['email'] . "\"";
+  $result = $con->query($sql_command);
   
-$result->close();
+  if ($row = $result->fetch_array()) {
+      if (password_verify($_POST['password'], $row['password']))
+      {
+          // Make sure the id is not a duplicate. This is unlikely. Also store in database.
+          while (!add_session_to_database($con, session_id()))
+              session_regenerate_id();
+          redirect_to_home();
+      }
+      else
+          login_error();
+  }
+  else
+      login_error();
+  
+  $result->close();
   
 ?>
-<html>
-  <body>
-    <?php
-      echo $password;
-      echo "<br/>";
-      echo $hash;
-      echo "<br/>";
-      $pass_good = password_verify($password, $hash);
-      //if ($pass_good)
-      //{
-      //// Make sure the id is not a duplicate. This is unlikely. Also store in database.
-      //    while (!add_session_to_database($con, session_id()))
-      //        session_regenerate_id();
-      //    redirect_to_home();
-      //}
-
-      if ($pass_good)
-          echo "Succcess!";
-      else
-          echo "Failure!";
-      ?>
-  </body>
-</html>
