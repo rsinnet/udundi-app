388d2fbfd0b49dba1b482fec3202d672a05560dd
Ryan W. Sinnet <ryan@rwsinnet.com>
2014-04-15 23:11:33 -0500

    Modifying log messages, prettier format.

--- inc/logging.php
+++ inc/logging.php
@@ -1,14 +1,17 @@
 <?php
 $debug_mode = true;
+$log_dir = "/home4/rsinnet/udundi/logs/";
 
 function do_log($msg, $type)
 {
+    global $log_dir;
     $bt = debug_backtrace();
     $caller = array_shift($bt);
-    $today = date("[Y-m-d H:i:s] ");
+    $today = date("Y-m-d H:i:s");
+    $lmsg = "$today $type ".$caller['file'].":".$caller['line'].": $msg\n";
 
-    error_log($today.$caller['file'].":".$caller['line']." ($type) $msg\n", 3, "/home4/rsinnet/udundi/logs/$type");
-    error_log($today.$caller['file'].":".$caller['line']." ($type) $msg\n", 3, "/home4/rsinnet/udundi/logs/ALL");
+    error_log($lmsg, 3, $log_dir.$type);
+    error_log($lmsg, 3, $log_dir."ALL");
 }
 
 function log_notice($msg)
