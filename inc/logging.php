b0d0626793b758c3fa158244d807c7a8517f566e
Ryan W. Sinnet <ryan@rwsinnet.com>
2014-04-15 20:55:07 -0500

    Adding logging.

--- inc/logging.php
+++ inc/logging.php
@@ -5,10 +5,10 @@ function log($msg, $type)
 {
     $bt = debug_backtrace();
     $caller = array_shift($bt);
-    $today = date("[Y-m-d H:i:s] ") 
+    $today = date("[Y-m-d H:i:s] ");
 
-    error_log($today.$caller['file'].":".$caller['line']." ($type) $msg", 3, '/home4/rsinnet/udundi/logs/$type');
-    error_log($today.$caller['file'].":".$caller['line']." ($type) $msg", 3, '/home4/rsinnet/udundi/logs/ALL');
+    error_log($today.$caller['file'].":".$caller['line']." ($type) $msg", 3, "home4/rsinnet/udundi/logs/$type");
+    error_log($today.$caller['file'].":".$caller['line']." ($type) $msg", 3, "/home4/rsinnet/udundi/logs/ALL");
 }
 
 function log_notice($msg)
