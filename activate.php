12ecab6e87f447272db25f5ec51d23145ba5875b
Ryan W. Sinnet <ryan@rwsinnet.com>
2014-04-15 22:08:44 -0500

    Working on activation service.

--- activate.php
+++ activate.php
@@ -6,23 +6,27 @@ $con = udundi_sql_connect();
 $token = $_GET["token"];
 
 $sql_command = "SELECT email FROM activations WHERE token=\"$token\"";
-$result = $con->query($sql_command);
+$result = execute_query($con, $sql_command);
 
 if ($row = $result->fetch_array())
 {
     // Enable and activate the account.
     $sql_command = "UPDATE users SET active=TRUE, enabled=TRUE WHERE email=\"$email\"";
-    $con->query($sql_command);
-    echo $sql_command;
+    if (!execute_query($con, $sql_command))
+    {
+        // TODO: Error Handling
+        log_error("Unable to activate and enable user `$email` in users table. ".
+                 mysqli_errno($scon) . " " . mysqli_error($scon));
+    }
 
     // Remove the activation nonce from the database.
     $sql_command = "DELETE FROM activations WHERE token=\"$token\"";
-    $con->query($sql_command);
-    echo "<br>";
-    echo $sql_command;
-    
-    echo $row["email"];
-    echo "<br>";
+    if (!execute_query($con, $sql_command))
+    {
+        // TODO: Error Handling
+        log_error("Unable to delete nonce from activations table. ".
+                 mysqli_errno($scon) . " " . mysqli_error($scon));
+    }
 }
 
 $result->close();
