31526019f95cd6a9a28f8f42c85a093314148ef6
Ryan W. Sinnet <ryan@rwsinnet.com>
2014-04-16 19:14:58 -0500

    Adding authentication before resending activation e-mail message.

--- resend_activation.php
+++ resend_activation.php
@@ -1,30 +1,55 @@
-<html>
-  <body>
-  <?php
+<?php
 
-  require_once("inc/secure.php");
+require_once("inc/secure.php");
 
-  $con = udundi_sql_connect();
-  $sql_command = "SELECT token FROM activations WHERE email=\"$email\"";
-  try
+if (empty($_POST['email']) || empty($_POST['password']) )
+  login_error();
+else
   {
-    $sth = execute_query($con, $sql_command);
-  }
-  catch (PDOException $ex)
-  {
-    log_warn("Unable to SELECT token from activations for `$email$`. ".
-             $ex->getMessage());
-  }
+    $email = $_POST['email'];
+    $password = $_POST['password'];
 
-  if ($row = $sth->fetch(PDO::FETCH_ASSOC))
-    { 
-      // Get activation token and resend message.
-      if ($row['token'])
-	{
-	  // TODO: Might consider authenticating the user before sending him an e-mail...
-	  send_activation_email($email, $token);
-	}
-    }
-  ?>
-  </body>
-</html>
+    // First verify user credentials.
+    try
+      {
+	$authentic = do_authentication($email, $password);
+      }
+    catch (InvalidLoginException $ex)
+      {
+        // TODO: Redirect to login page with email field filled out.
+        echo $ex->getMessage();
+      }
+
+    if ($authentic)
+      {
+	// Now get the activation token.
+	$con = udundi_sql_connect();
+	$sql_command = "SELECT token FROM activations WHERE email=\"$email\"";
+	try
+	  {
+	    $sth = execute_query($con, $sql_command);
+	  }
+	catch (PDOException $ex)
+	  {
+	    log_warn("Unable to SELECT token from activations for `$email$`. ".
+		     $ex->getMessage());
+	  }
+
+	if ($row = $sth->fetch(PDO::FETCH_ASSOC))
+	  { 
+	    // Get activation token and resend message.
+	    if ($row['token'])
+	      {
+		// TODO: Might consider authenticating the user before sending him an e-mail...
+		send_activation_email($email, $token);
+	      }
+	  }
+      }
+    else
+      {
+	// TODO: This is a bad error, it means the form was submitted wrong or we have a hacker.
+	log_warn("resend_activation.php reached without proper credentials. Possible security breach.");
+	login_error();
+      }
+  }
+?>
