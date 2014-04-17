466cf8dcb0709280d52a3cd31b78cf982604769b
nparker <nathan@udundi.com>
2014-04-17 18:42:53 -0500

    More changes to pages

--- inc/header.php
+++ inc/header.php
@@ -1,9 +1,8 @@
 <!DOCTYPE html>
 <html lang="en-us">
 	<head>
-		<meta charset="utf-8">
-		<!--<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">-->
-
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />	
 		<title> <?php echo $page_title != "" ? $page_title." - " : ""; ?> Udundi Analytics </title>
 		<meta name="description" content="">
 		<meta name="author" content="">
