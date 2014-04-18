9b25e6dcae6f0129f71229aa6380fd04333630b6
nparker <nathan@udundi.com>
2014-04-17 20:09:31 -0500

    Twitter page added

---
+++ twitter.php
@@ -0,0 +1,89 @@
+<?php
+
+//initilize the page
+require_once("inc/init.php");
+
+//require UI configuration (nav, ribbon, etc.)
+require_once("inc/config.ui.php");
+
+/*---------------- PHP Custom Scripts ---------
+
+YOU CAN SET CONFIGURATION VARIABLES HERE BEFORE IT GOES TO NAV, RIBBON, ETC.
+E.G. $page_title = "Custom Title" */
+
+$page_title = "Twitter";
+
+/* ---------------- END PHP Custom Scripts ------------- */
+
+//include header
+//you can add your custom css in $page_css array.
+//Note: all css files are inside css/ folder
+$page_css[] = "your_style.css";
+include("inc/header.php");
+
+//include left panel (navigation)
+//follow the tree in inc/config.ui.php
+$page_nav["twitter"]["active"] = true;
+include("inc/nav.php");
+
+?>
+<!-- ==========================CONTENT STARTS HERE ========================== -->
+<!-- MAIN PANEL -->
+<div id="main" role="main">
+	<?php
+		//configure ribbon (breadcrumbs) array("name"=>"url"), leave url empty if no url
+		//$breadcrumbs["New Crumb"] => "http://url.com"
+		include("inc/ribbon.php");
+	?>
+
+	<!-- MAIN CONTENT -->
+	<div id="content">
+
+		<div class="row">
+			<div class="col-xs-12 col-sm-9 col-md-9 col-lg-6">
+				<h1 class="page-title txt-color-blueDark"><i class="fa-fw fa fa-twitter"></i>Twitter</h1>
+			</div>
+		</div>
+		<!-- widget grid -->
+		<section id="widget-grid" class="">
+
+
+			<!-- row -->
+
+
+
+
+
+		</section>
+		<!-- end widget grid -->
+
+	</div>
+	<!-- END MAIN CONTENT -->
+
+</div>
+<!-- END MAIN PANEL -->
+<!-- ==========================CONTENT ENDS HERE ========================== -->
+
+<?php 
+	//include required scripts
+	include("inc/scripts.php"); 
+?>
+
+<!-- PAGE RELATED PLUGIN(S) 
+<script src="..."></script>-->
+<!-- Flot Chart Plugin: Flot Engine, Flot Resizer, Flot Tooltip -->
+<script src="<?php echo ASSETS_URL; ?>/js/plugin/flot/jquery.flot.cust.js"></script>
+<script src="<?php echo ASSETS_URL; ?>/js/plugin/flot/jquery.flot.resize.js"></script>
+<script src="<?php echo ASSETS_URL; ?>/js/plugin/flot/jquery.flot.tooltip.js"></script>
+
+<!-- Vector Maps Plugin: Vectormap engine, Vectormap language -->
+<script src="<?php echo ASSETS_URL; ?>/js/plugin/vectormap/jquery-jvectormap-1.2.2.min.js"></script>
+<script src="<?php echo ASSETS_URL; ?>/js/plugin/vectormap/jquery-jvectormap-world-mill-en.js"></script>
+
+<!-- Full Calendar -->
+<script src="<?php echo ASSETS_URL; ?>/js/plugin/fullcalendar/jquery.fullcalendar.min.js"></script>
+
+<?php 
+	//include footer
+	include("inc/footer.php"); 
+?>
