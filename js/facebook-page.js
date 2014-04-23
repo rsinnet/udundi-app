298370844c4e618224291d340919845e613dabf0
Ryan W. Sinnet <ryan@rwsinnet.com>
2014-04-23 15:04:43 -0500

    Removed weird typo.

--- js/facebook-page.js
+++ js/facebook-page.js
@@ -3,7 +3,7 @@
 /* chart colors default */
 var $chrt_border_color = "#efefef";
 var $chrt_grid_color = "#DDD"
-var $chrt_main = "#E24913";    else
+var $chrt_main = "#E24913";
 /* red       */
 var $chrt_second = "#6595b4";
 /* blue      */
