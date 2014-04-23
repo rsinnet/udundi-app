27e6b5110a4b92d2f93b83f0a997b6fc9bb39e52
Ryan W. Sinnet <ryan@rwsinnet.com>
2014-04-23 14:22:15 -0500

    Working on date ranges in facebook--python interface.

--- py/fpe_interface.py
+++ py/fpe_interface.py
@@ -3,7 +3,7 @@
 import facebook
 from facebook_python_extension import GraphApiExtension as G
 
-import datetime, iso8601
+import datetime, iso8601, time
 
 import json
 import cgi, cgitb
@@ -16,8 +16,8 @@ access_token = data.getvalue('user_access_token')
 
 edge = data.getvalue('edge');
 period = data.getvalue('period');
-since = data.getvalue('since');
-until = data.getvalue('until');
+since = time.strptime(data.getvalue('since'), '%Y-%m-%d')
+until = time.strptime(data.getvalue('until'), '%Y-%m-%d')
 
 graph = G(access_token)
 
@@ -36,10 +36,10 @@ while graph.paginated() and new_data:
     current_data = graph.previous()[0]['values']
     for j in range(len(current_data)):
         item_date = iso8601.parse_date(current_data[j]['end_time'])
-        if item_date > since_date:
+        if item_date > since:
             data['values'] += current_data[j]
             new_data = True
-        elif item_date > until_date:
+        elif item_date > until:
             new_data = True
 
 # Print out the content of the message.
