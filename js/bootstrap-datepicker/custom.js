b241a21b26b5154aef97b9e1e0aaa670dcba00ac
nparker <nathan@udundi.com>
2014-04-23 16:00:29 -0500

    chnages in script tag for custom js

--- js/bootstrap-datepicker/custom.js
+++ js/bootstrap-datepicker/custom.js
@@ -1,4 +1,3 @@
-      <script>
         $(function(){
           var defaults = {},
               defaults_form = $('<form>', {html: $('.sandbox-form').html()})
@@ -244,5 +243,3 @@
           ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
           var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
         })();
-
-      </script>
