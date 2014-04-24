19a28d9fc1cbca917cdaee5c63cf4134a7510c1f
nparker <nathan@udundi.com>
2014-04-23 19:43:02 -0500

    changes in button col size

--- facebook.php
+++ facebook.php
@@ -62,7 +62,7 @@
 	<input type="text" class="input-sm form-control" id="until_date" placeholder="4/24/2014" />
       </div>
       
-      <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+      <div class="col-xs-6 col-sm-2 col-md-2 col-lg-2">
 	<button type="button" class="btn btn-primary" id="update_button">Update</button>
       </div>
     </div>
