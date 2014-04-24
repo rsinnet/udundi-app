243ce0afb2833a9c5e2cbcfffec9b9f313b8771b
Ryan W. Sinnet <ryan@rwsinnet.com>
2014-04-23 19:07:15 -0500

    Adjusting Go! button position.

--- facebook.php
+++ facebook.php
@@ -60,14 +60,16 @@
 	  </section>
 	</form>
 	
-	<form class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+	<form>
 	  <div class="input-daterange input-group col-xs-12 col-sm-6 col-md-6 col-lg-4" id="datepicker">
 	    <input type="text" class="input-sm form-control" id="since_date" placeholder="4/23/2014"/>
 	    <span class="input-group-addon">to</span>
 	    <input type="text" class="input-sm form-control" id="until_date" placeholder="4/24/2014" />
 	  </div>
 	  
-	  <button type="button" class="input-sm form-control" id="update_button">Go!</button>
+	  <span class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+	    <button type="button" class="input-sm form-control" id="update_button">Go!</button>
+	  </span>
 	</form>
       </div>
     </div>
