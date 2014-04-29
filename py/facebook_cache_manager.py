3f3a68741d05c964fcd7965bb784a7b8c9f6fcc7
Ryan W. Sinnet <ryan@rwsinnet.com>
2014-04-29 11:06:38 -0500

    Forgot to comment out a line.

--- py/facebook_cache_manager.py
+++ py/facebook_cache_manager.py
@@ -133,7 +133,7 @@ class UdundiUser():
         insight_subsql = '(SELECT id FROM facebook_insights_names ' + \
             'WHERE insight_name="%s")'
         #insight_subsql = '(SELECT id FROM facebook_insights_names ' + \
-            'WHERE insight_name="{0}")'.format(insight)
+        #   'WHERE insight_name="{0}")'.format(insight)
         #sql_command = 'REPLACE INTO facebook_insights_basic ' + \
         #    '(userid, insightid, period, end_time, value) VALUES ' + \
         #    ', '.join(['({0}, {1}, "{2}", "{3}", {4})'.format(\
